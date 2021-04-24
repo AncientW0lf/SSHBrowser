ba0495498eb461f4814454756d5f06bbb2eca623
LunarDiver <25Prozent@gmail.com>
2021-04-24 23:19:35 +0200

    Added clickable links to listed directories.

--- SSHBrowser/wwwroot/js/index.js
+++ SSHBrowser/wwwroot/js/index.js
@@ -1,3 +1,6 @@
-function goToPath() {
-    window.location.href = "/?p=" + encodeURIComponent($("#pathInput")[0].value);
+function goToPath(path) {
+    if (!path)
+        window.location.href = "/?p=" + encodeURIComponent($("#pathInput")[0].value);
+    else
+        window.location.href = "/?p=" + encodeURIComponent(path.replace("//", "/"));
 }
