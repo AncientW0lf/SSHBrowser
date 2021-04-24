1a631b6889919a5fe0ddfa777f38130fd3da1bfc
LunarDiver <25Prozent@gmail.com>
2021-04-24 19:16:05 +0200

    Added unit test project for future tests.

---
+++ SSHClient.Tests/ClientTest.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SSHClient.Tests
+{
+    [TestClass]
+    public class ClientTest
+    {
+        [TestMethod]
+        public void TestConnection()
+        {
+
+        }
+    }
+}
